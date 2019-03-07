8c80fe2fb13beca0a73740224d4d3dee34c6d1d6
Daniel Weinman <namniewd@gmail.com>
2019-03-07 13:56:54 -0500

    adds production environment for db

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,14 @@
 // Update with your config settings.
 
+const localPgConnection = {
+  host: 'localhost',
+  database: 'lambda',
+  user: 'student',
+  password: 'hired!'
+
+}
+const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -27,4 +36,14 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: prodDbConnection, //object or string
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  }
 };
